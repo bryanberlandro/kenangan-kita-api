c6018396ff123af7d6d45a1d992f4aeac53ae928
Berlandro <bryangodlive0@gmail.com>
2024-12-18 19:12:28 +0700

    fix a bit of code

--- index.js
+++ index.js
@@ -20,7 +20,93 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.status(200).json({
+        message: {
+            endpoint: [
+                {
+                    table: [
+                        {
+                            path: "/tables",
+                            desc: "get all tables",
+                            method: "GET"
+                        },
+                        {
+                            path: "/add-table",
+                            desc: "post table",
+                            method: "POST"
+                        },
+                    ]
+                },
+                {
+                    review: [
+                        {
+                            path: "/reviews",
+                            desc: "get all reviews",
+                            method: "GET"
+                        },
+                        {
+                            path: "/create-review",
+                            desc: "Post Review",
+                            method: "POST"
+                        }
+                    ]
+                },
+                {
+                    order: [
+                        {
+                            path: "/orders",
+                            desc: "get all orders by status",
+                            method: "GET"
+                        },
+                        {
+                            path: "/total-income",
+                            desc: "get total orders income",
+                            method: "GET"
+                        },
+                        {
+                            path: "/create-order",
+                            desc: "Post new order",
+                            method: "POST"
+                        },
+                        {
+                            path: "/update-order",
+                            desc: "Update existing order",
+                            method: "PUT"
+                        },
+                        {
+                            path: "/delete-order",
+                            desc: "Delete existing order",
+                            method: "DELETE"
+                        }
+                    ]
+                },
+                {
+                    menu: [
+                        {
+                            path: "/menus",
+                            desc: "Get all menus",
+                            method: "GET"
+                        },
+                        {
+                            path: "/create-menu",
+                            desc: "Create menu",
+                            method: "POST"
+                        },
+                        {
+                            path: "/menus/category/:category",
+                            desc: "Get all menus by category",
+                            method: "GET"
+                        },
+                        {
+                            path: "/menus/search",
+                            desc: "Search menu by name",
+                            method: "GET"
+                        }
+                    ]
+                }
+            ]
+        }
+    })
 });
 
 app.use(router)
